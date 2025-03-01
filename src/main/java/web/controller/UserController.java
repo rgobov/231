34ff86870c3fd52c3d3aecbcbc60aafb283c2625
rgobov <rgobov@gmail.com>
2025-03-01 20:48:14 +0200

    4

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -14,7 +14,7 @@ import web.service.UserServiceImpl;
 public class UserController {
 
     @Autowired
-    private UserServiceImpl userService;
+    private UserService userService;
 
 
     @GetMapping()
