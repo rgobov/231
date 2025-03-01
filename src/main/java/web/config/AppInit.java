3fa22a669692765ea1dec3fa0132cafed5df98eb
rgobov <rgobov@gmail.com>
2025-03-01 21:41:10 +0200

    ru

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -1,8 +1,17 @@
 package web.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.Filter;
+
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return new Filter[]{characterEncodingFilter};}
 
     // Метод, указывающий на класс конфигурации
     @Override
